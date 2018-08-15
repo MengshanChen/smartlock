e40b7813088ed4a356c5ebb3735417f1cfb3765a
MengshanChen <chenm17@seattleu.edu>
2018-08-14 23:14:07 -0700

    Finish static pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_15_034403) do
+ActiveRecord::Schema.define(version: 2018_08_15_054311) do
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.string "email"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -15,7 +15,7 @@ class UsersTest < ApplicationSystemTestCase
     click_on "New User"
 
     fill_in "Email", with: @user.email
-    fill_in "Password", with: @user.password
+    fill_in "Name", with: @user.name
     click_on "Create User"
 
     assert_text "User was successfully created"
@@ -27,7 +27,7 @@ class UsersTest < ApplicationSystemTestCase
     click_on "Edit", match: :first
 
     fill_in "Email", with: @user.email
-    fill_in "Password", with: @user.password
+    fill_in "Name", with: @user.name
     click_on "Update User"
 
     assert_text "User was successfully updated"
