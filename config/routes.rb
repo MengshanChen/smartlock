c04492b90d63dc50b3fd44467520ea0cadc292f6
MengshanChen <chenm17@seattleu.edu>
2018-08-16 10:56:10 -0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
   get 'users/new'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,8 +2,20 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-  end
+  get root_path
+  assert_response :success
+  assert_select "title", "Home | Smart Lock"
+end
 
+test "should get about" do
+  get about_path
+  assert_response :success
+  assert_select "title", "About | Smart Lock"
+end
+
+test "should get contact" do
+  get contact_path
+  assert_response :success
+  assert_select "title", "Contact | Smart Lock"
+end
 end
